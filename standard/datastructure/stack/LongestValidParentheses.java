ecba0e86b17a672369188ae84b3077596ced2ddf
shafa112 <shafa.hassan112@gmail.com>
2024-05-15 15:00:25 +0530

    longest valid parenthesis

--- standard/datastructure/stack/LongestValidParentheses.java
+++ standard/datastructure/stack/LongestValidParentheses.java
@@ -6,12 +6,16 @@ import java.util.Stack;
 // )()())
 // ()(()(
 
-/**
- * Here for every closing brace ')', we calculate length of possible
- * valid parenthesis onto its immediate length
- * max = Math.max(max, length of valid parenthesis on left of closing brace)
- */
 public class LongestValidParentheses {
+
+
+
+    /**
+     * Here for every closing brace ')', we calculate length of possible
+     * valid parenthesis onto its immediate length
+     * max = Math.max(max, length of valid parenthesis on left of closing brace)
+     * time : O(n), space : O(n)
+     */
     public static int longestValidParentheses(String str) {
         int length=0;
         int max=0;
@@ -25,6 +29,46 @@ public class LongestValidParentheses {
                 if(s.isEmpty()) s.push(i);
                 else length = i - s.peek();
                 max = Math.max(length,max);
+            }
+        }
+        return max;
+    }
+
+
+    /*
+    *
+    * Valid parenthesis will consists of equal number of open and closed brackets
+    * When we travel from left to right, we keep a count of left and right bracket
+    * when they are equal we calculate length
+    * If closing bracket is more than open then we set open and left bracket count to 0
+    *
+    * Time : O(n), space: O(1)
+    * */
+    public static int longestValidParenthesis(String str) {
+        int max = 0;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)=='(') ++left;
+            else {
+                ++right;
+            }
+            if(right==left) max = Math.max(left+right,max);
+            if(right>left) {
+                left=right=0;
+            }
+        }
+
+        left = right = 0;
+        for (int i = str.length()-1; i >=0 ; i--) {
+            if(str.charAt(i)=='(') ++left;
+            else {
+                ++right;
+            }
+
+            if(right==left) max = Math.max(left+right,max);
+            if(left>right) {
+                left=right=0;
             }
         }
         return max;
