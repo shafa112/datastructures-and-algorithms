36e9fd5762e6df622bf994c9f15f3ddd3b8c1e20
shafa112 <shafa.hassan112@gmail.com>
2024-05-31 22:12:27 +0530

    longest common subseq  len

--- standard/algorithm/dynamicprogramming/LongestCommonSubsequence.java
+++ standard/algorithm/dynamicprogramming/LongestCommonSubsequence.java
@@ -11,51 +11,24 @@ public class LongestCommonSubsequence {
      * Time : O(2^(m+n))
      * Space: m+n
      * */
-    public int longestCommonSubsequence1(String text1, String text2) {
-        return LCSubsequence1(text1,text2,0,0);
+    public int longestCommonSubseq(String s1, String s2) {
+        int[][] dp = new int[s1.length()][s2.length()];
+        for (int[] a: dp) Arrays.fill(a,-1);
+        return longestCommonSubseq(s1, s1.length() - 1, s2, s2.length() - 1,dp);
     }
 
-    public int LCSubsequence1(String text1, String text2, int i, int j) {
-        if(i>=text1.length() || j>=text2.length()) return 0;
+    public int longestCommonSubseq(String s1, int n1, String s2, int n2, int[][] dp) {
+        if(n1 < 0 || n2 < 0) return 0;
+        if(dp[n1][n2]!=-1) return dp[n1][n2];
         int len = 0;
-        if(text1.charAt(i) == text2.charAt(j)) {
-            len =  1 + LCSubsequence1(text1, text2, i+1, j+1);
+        if (s1.charAt(n1) == s2.charAt(n2)) {
+            len = 1 + longestCommonSubseq(s1, n1 - 1, s2, n2 - 1,dp);
         } else {
-            len = Math.max(
-                    LCSubsequence1(text1,text2,i,j+1),
-                    LCSubsequence1(text1,text2,i+1,j)
-            );
+            len = Math.max(longestCommonSubseq(s1,n1-1,s2,n2,dp),longestCommonSubseq(s1,n1,s2,n2-1,dp));
         }
-        return len;
+        return dp[n1][n2] = len;
     }
 
-    /*
-     * Dp with Recursive Solution:
-     * Time : O(m*n)
-     * Space: m+n + m*n
-     * */
-    public int longestCommonSubsequence2(String text1, String text2) {
-        int[][] dp = new int[text1.length()][text2.length()];
-        for (int[] a:dp) {
-            Arrays.fill(a,-1);
-        }
-        return LCSubsequence2(text1,text2,0,0,dp);
-    }
-
-    public int LCSubsequence2(String text1, String text2, int i, int j,int[][] dp) {
-        if(i>=text1.length() || j>=text2.length()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int len = 0;
-        if(text1.charAt(i) == text2.charAt(j)) {
-            len =  1 + LCSubsequence2(text1, text2, i+1, j+1,dp);
-        } else {
-            len = Math.max(
-                    LCSubsequence2(text1,text2,i,j+1,dp),
-                    LCSubsequence2(text1,text2,i+1,j,dp)
-            );
-        }
-        return dp[i][j]=len;
-    }
 
     // DP with tabulation //removes the auxiliary stack space
     // Time: mn
@@ -69,22 +42,22 @@ public class LongestCommonSubsequence {
      *            max(dp[i][j-1], dp[i-1][j]) , if s1[i]!=s2[j]
      *          }
      */
-    public static int lcs(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+    public static int longestCommonSubsequenceIterative(String s1, String s2) {
+        int n1 = s1.length() + 1;
+        int n2 = s2.length() + 1;
+        int[][] dp = new int[n1][n2];
+        for (int i = 1; i < n1; i++) {
+            for (int j = 1; j < n2; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        Util.printArray(dp);
-        return dp[s1.length()][s2.length()];
+        return dp[n1-1][n2-1];
     }
 
     public static void main(String[] args) {
         String s1 = "bb";
         String s2 = "jb";
-        System.out.println(lcs(s1, s2));
+        System.out.println(longestCommonSubsequenceIterative(s1, s2));
     }
 }
