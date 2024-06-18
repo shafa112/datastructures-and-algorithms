1c2945bb2ef804218e8de4d6433afbcc4567bb0a
shafa112 <shafa.hassan112@gmail.com>
2024-06-18 12:22:16 +0530

    updated stock span

--- standard/datastructure/stack/StockSpan.java
+++ standard/datastructure/stack/StockSpan.java
@@ -1,37 +1,12 @@
 package datastructure.stack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
 // https://www.geeksforgeeks.org/the-stock-span-problem/
 public class StockSpan {
 
-    public static int[] calculateSpan(int price[]) {
-        int span[] = new int[price.length];
-        Stack<Integer> stack = new Stack<>();
-        for(int i = price.length-1;i >= 0; i--) {
-            span[i]=1;
-            if(stack.isEmpty()) {
-                stack.push(i);
-            } else {
-                if(price[i]<price[stack.peek()]) stack.push(i);
-                else {
-                    while(!stack.isEmpty() && price[i]>price[stack.peek()]) {
-                        int index = stack.pop();
-                        if(!stack.isEmpty()) span[stack.peek()]+=span[index];
-                    }
-                    stack.push(i);
-                }
-            }
-        }
-        while(!stack.isEmpty()) {
-            int index = stack.pop();
-            if(!stack.isEmpty()) span[stack.peek()]+=span[index];
-        }
-        return span;
-    }
-
-    //Better solution
     // span is greater than or equal to
     // remove stack elements until all lesser elements are removed
     // for any element at index i , span = i - (stack.peek..which will be just greater element than element at i)
@@ -56,6 +31,11 @@ public class StockSpan {
         int price[] = {74,665,742,512};
         int span[] = getSpan(price);
         System.out.println(Arrays.toString(span));
+        ArrayList<Integer> a = new ArrayList<>();
+        a.size();
+        a.add(1);
+        System.out.println(a.get(a.size()-1));
+
 //        System.out.println(Arrays.toString(price));
 //        for(int i = 0; i < price.length; i++) {
 //            System.out.println(span[i] + ", " + price[i]);
